8545c2975af43e4c11ff49b371764a6d04f4ce90
Yoosuf520 <yoosuf.ahamed10@gmail.com>
2026-07-01 18:33:22 +0530

    cicd added

--- yt-chrome-plugin-frontend/popup.js
+++ yt-chrome-plugin-frontend/popup.js
@@ -2,9 +2,9 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
   const outputDiv = document.getElementById("output");
-  const API_KEY = "Google Cloud Youtube Data Api";  // Replace with your actual YouTube Data API key
+  const API_KEY = "AIzaSyDZ5O1Wv-0E5deA9WPJAFEZlfEkVIOUmfs";  // Replace with your actual YouTube Data API key
   // const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80';   
-  const API_URL = 'http://3.110.185.110:5000'; // Removed trailing slash to prevent double slashes in paths (if i want to run locally i should add my local post insten of EC2 uri)
+  const API_URL = 'http://3.110.185.110:8000'; // Removed trailing slash to prevent double slashes in paths (if i want to run locally i should add my local post insten of EC2 uri)
 
   // Get the current tab's URL
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
